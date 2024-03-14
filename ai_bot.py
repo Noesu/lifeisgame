032d1750d65476fa0980f20cfe235e056269239f
Noesu <138776947+Noesu@users.noreply.github.com>
2024-03-14 19:06:43 +0530

    SQLite, pagination, edit, delete functional added
    TODO:
    Finalize add_question() to use same FSM as edit_question()
    Set up question usage functional
    Add posting functional
    Add timer functional

--- ai_bot.py
+++ ai_bot.py
@@ -12,30 +12,39 @@ from aiogram.types import (KeyboardButton,
                            InlineKeyboardButton,
                            ReplyKeyboardMarkup)
 import config
-import database
+import sqlite3 as sq
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.ERROR)
 
 bot = Bot(token=config.TOKEN, parse_mode="HTML")
 dp = Dispatcher()
 
+Q_RANGE = 10
+
 
 class Question(StatesGroup):
-    q_number = State()
-    set_q_text = State()
-    set_a_text = State()
+    rowid = State()
+    q_used = State()
+    q_text = State()
+    a_text = State()
+    old_q = State()
+    old_a = State()
 
 
 @dp.message(CommandStart())
 async def command_start(message: types.Message):
+    with sq.connect("questions.db") as db:
+        cur = db.cursor()
+        cur.execute("SELECT COUNT (*) FROM questions")
+        count = cur.fetchone()[0]
     menu_message = (f'<b>Life is game by <i>George Bars</i></b>\n'
                     f'Система управления квизами v 0.1\n\n'
-                    f'Всего сохранено вопросов: {len(database.questions)}\n'
+                    f'Всего сохранено вопросов: {count}\n'
                     f'Ближайший квиз запланирован на DD.Month HH:MM')
     builder = InlineKeyboardBuilder()
     builder.add(types.InlineKeyboardButton(
         text="База данных",
-        callback_data="database"))
+        callback_data="database_"))
     builder.add(types.InlineKeyboardButton(
         text="Управление",
         callback_data="settings"))
@@ -43,112 +52,153 @@ async def command_start(message: types.Message):
     # await callback.answer()
 
 
-# @dp.callback_query(F.data == "database")
-# async def edit_database(callback: types.CallbackQuery):
-#     menu_message = f'Всего сохранено вопросов: {len(database.questions)}'
-#     builder = InlineKeyboardBuilder()
-#     builder.add(types.InlineKeyboardButton(
-#         text="Просмотр вопросов",
-#         callback_data="read_database"))
-#     builder.add(types.InlineKeyboardButton(
-#         text="Добавление вопроса",
-#         callback_data="add_question"))
-#     await callback.message.answer(menu_message, reply_markup=builder.as_markup())
-#     await callback.answer()
-
-
-@dp.callback_query(F.data == "database")
-async def read_database(callback: types.CallbackQuery):
-    q_index = 0
-    q_range = 10
-    menu_message = "".join(
-        f'{index + 1}. {q_text[:40]}\n' for index, (q_text, a_text, pub_times) in
-        enumerate(database.questions[q_index:q_index + q_range]))
+@dp.callback_query(lambda callback_query: callback_query.data.startswith('database_'))
+async def read_database(callback: types.CallbackQuery, q_offset: int = 0):
     builder = InlineKeyboardBuilder()
-    for i in range(q_index, min(q_range, len(database.questions))):
-        builder.add(types.InlineKeyboardButton(text=str(i + 1), callback_data=f"question_{i + 1}"))
+
+    with sq.connect("questions.db") as db:
+        cur = db.cursor()
+        cur.execute("SELECT COUNT (*) FROM questions")
+        count = cur.fetchone()[0]
+
+    match callback.data.split('_')[1]:
+        case 'prev':
+            q_offset = max(0, q_offset-Q_RANGE)
+        case 'next':
+            q_offset = min(q_offset+Q_RANGE, count-Q_RANGE)
+        case _:
+            q_offset = 0
+
+    with sq.connect("questions.db") as db:
+        cur = db.cursor()
+        cur.execute("SELECT Question, rowid FROM questions ORDER BY rowid ASC LIMIT ? OFFSET ?", (Q_RANGE, q_offset))
+        questions = cur.fetchall()
+
+    menu_message = ""
+    for index, (q_text, rowid) in enumerate(questions, q_offset):
+        menu_message += f'{index + 1}. {q_text[:40]}\n'
+        builder.add(types.InlineKeyboardButton(text=str(index + 1), callback_data=f"question_{rowid}"))
     builder.adjust(5)
-    if q_index > 0:
-        prev_btn = types.InlineKeyboardButton(text="<<< 10", callback_data="prev_10")
-    else:
-        prev_btn = types.InlineKeyboardButton(text="<<< 10", callback_data="prev_10")
-    edit_btn = types.InlineKeyboardButton(text="Edit", callback_data="edit_question")
-    if len(database.questions) > q_index + q_range:
-        next_btn = types.InlineKeyboardButton(text="10 >>>", callback_data="next_10")
-    else:
-        next_btn = types.InlineKeyboardButton(text="10 >>>", callback_data="next_10")
-    builder.row(prev_btn, edit_btn, next_btn)
+
+    prev_btn = types.InlineKeyboardButton(text=f"<<< {Q_RANGE}", callback_data="database_prev")
+    next_btn = types.InlineKeyboardButton(text=f"{Q_RANGE} >>>", callback_data="database_next")
+    builder.row(prev_btn, next_btn)
+
+    add_q = types.InlineKeyboardButton(text=f"Добавить вопрос", callback_data="add_question")
+    builder.row(add_q)
+
     await callback.message.answer(menu_message, reply_markup=builder.as_markup())
     await callback.answer()
 
 
 @dp.callback_query(lambda callback_query: callback_query.data.startswith('question_'))
 async def read_question(callback: types.CallbackQuery, state: FSMContext):
-    Question.q_number = int(callback.data.split('_')[1]) - 1
-    await state.update_data(q_number=Question.q_number)
-    menu_message = f'<b>Вопрос №{Question.q_number + 1}</b>\n\n' \
-                   f'Вопрос: <code>{database.questions[Question.q_number][0]}</code>\n' \
-                   f'Ответ: <code>{database.questions[Question.q_number][1]}</code>\n' \
-                   f'Количество публикаций: {database.questions[Question.q_number][2]}'
     builder = InlineKeyboardBuilder()
-    edit_q = types.InlineKeyboardButton(text="Редактировать вопрос", callback_data=f"editq_{Question.q_number}")
-    edit_a = types.InlineKeyboardButton(text="Редактировать ответ", callback_data=f"edit_a{Question.q_number}")
-    del_q = types.InlineKeyboardButton(text="Удалить вопрос", callback_data=f"del_q{Question.q_number}")
-    clear_q_flag = types.InlineKeyboardButton(text="Сбросить признак", callback_data=f"clear_q_flag{Question.q_number}")
-    send_q = types.InlineKeyboardButton(text="Опубликовать", callback_data=f"send_q{Question.q_number}")
-    builder.add(edit_q, edit_a, del_q, clear_q_flag).adjust(2)
+    rowid = int(callback.data.split('_')[1])
+    print(rowid)
+    await state.update_data(rowid=rowid)
+    with sq.connect("questions.db") as db:
+        cur = db.cursor()
+        cur.execute("SELECT Question, Answer, Used FROM Questions WHERE rowid = ?", (rowid,))
+        row = cur.fetchone()
+    await state.update_data(old_q=row[0])
+    await state.update_data(old_a=row[1])
+    menu_message = f'Вопрос: <code>{row[0]}</code>\n' \
+                   f'Ответ: <code>{row[1]}</code>\n' \
+                   f'Опубликован: {row[2]}'
+    edit_q = types.InlineKeyboardButton(text="Редактировать вопрос", callback_data=f"edit_q")
+    del_q = types.InlineKeyboardButton(text="Удалить вопрос", callback_data=f"del_q")
+    clear_q_flag = types.InlineKeyboardButton(text="Сбросить признак", callback_data=f"clear_q_flag")
+    send_q = types.InlineKeyboardButton(text="Опубликовать", callback_data=f"send_q{rowid}")
+    builder.add(edit_q, del_q, clear_q_flag).adjust(2)
     builder.row(send_q)
     await callback.message.answer(menu_message, reply_markup=builder.as_markup())
     await callback.answer()
 
 
-@dp.callback_query(lambda callback_query: callback_query.data.startswith('editq_'))
+@dp.callback_query(lambda callback_query: callback_query.data.startswith('edit_q'))
 async def edit_question(callback: types.CallbackQuery, state: FSMContext) -> None:
     await callback.message.answer("Введите вопрос:", reply_markup=ReplyKeyboardRemove())
-    await state.set_state(Question.set_q_text)
+    await state.set_state(Question.q_text)
 
 
-@dp.message(Question.set_q_text)
+@dp.message(Question.q_text)
 async def question_set(message: Message, state: FSMContext):
-    new_q = message.text
-    builder = InlineKeyboardBuilder()
+    q_text = message.text
+    await state.update_data(q_text=q_text)
+    await message.answer("Введите ответ:", reply_markup=ReplyKeyboardRemove())
+    await state.set_state(Question.a_text)
+
+
+@dp.message(Question.a_text)
+async def question_check(message: Message, state: FSMContext):
+    a_text = message.text
+    await state.update_data(a_text=a_text)
     data = await state.get_data()
-    q_number = data.get('q_number')
-    menu_message = f'<b>Вопрос №{q_number + 1}</b>\n\n' \
-                   f'Старый вопрос: <code>{database.questions[Question.q_number][0]}</code>\n' \
-                   f'Новый вопрос: <code>{new_q}</code>\n'
-    builder.add(types.InlineKeyboardButton(text="Отмена", callback_data=f"question_{q_number+1}"))
-    builder.add(types.InlineKeyboardButton(text="Сохранить", callback_data=f"save_question_{q_number+1}"))
-    await state.update_data(new_q=new_q)
+    menu_message = f'Старый вопрос: <code>{data.get("old_q")}</code>\n' \
+                   f'Новый вопрос: <code>{data.get("q_text")}</code>\n\n' \
+                   f'Старый ответ: <code>{data.get("old_a")}</code>\n' \
+                   f'Новый ответ: <code>{a_text}</code>'
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(text="Отмена", callback_data=f'question_{data.get("rowid")}'))
+    builder.add(types.InlineKeyboardButton(text="Сохранить", callback_data=f'save_question'))
     await message.answer(text=menu_message, reply_markup=builder.as_markup())
     await state.set_state()
 
 
-@dp.callback_query(lambda callback_query: callback_query.data.startswith('save_question_'))
+@dp.callback_query(F.data == "save_question")
 async def save_question(callback: types.CallbackQuery, state: FSMContext):
-    Question.q_number = int(callback.data.split('_')[1]) - 1
-    database.questions[]
+    data = await state.get_data()
+    with sq.connect("questions.db") as db:
+        cur = db.cursor()
+        cur.execute("UPDATE questions SET Question = ?, Answer = ?, Used = ? WHERE rowid = ?",
+                    (data.get('q_text'), data.get('a_text'), 0, data.get('rowid')))
+        db.commit()
+    builder = InlineKeyboardBuilder()
+    menu_message = "Вопрос сохранён в базу данных"
+    builder.add(types.InlineKeyboardButton(text="Назад", callback_data=f"question_{data.get('rowid')}"))
     await callback.message.answer(menu_message, reply_markup=builder.as_markup())
     await callback.answer()
 
-# @dp.callback_query(F.data == "add_question")
-# async def send_random_value(callback: types.CallbackQuery, state: FSMContext) -> None:
-#     await state.set_state(Question.q_text)
-#     await callback.answer("Введите вопрос:", reply_markup=ReplyKeyboardRemove())
-#
-#
-# @dp.message(Question.q_text)
-# async def process_name(message: Message, state: FSMContext) -> None:
-#     await state.update_data(name=message.text)
-#     await state.set_state(Question.a_text)
-#     await message.answer(f"Введите ответ:", reply_markup=ReplyKeyboardRemove())
-#
-#
-# @dp.message(Question.a_text)
-# async def process_name(message: Message, state: FSMContext) -> None:
-#     await state.update_data(name=message.text)
-#     await state.set_state(Question.a_text)
-#     await message.answer(f"Введите ответ:", reply_markup=ReplyKeyboardRemove())
+
+@dp.callback_query(F.data == "add_question")
+async def add_question(callback: types.CallbackQuery, state: FSMContext):
+    await state.clear()
+    with sq.connect("questions.db") as db:
+        cur = db.cursor()
+        cur.execute("INSERT INTO questions DEFAULT VALUES")
+        cur.execute("SELECT last_insert_rowid()")
+        rowid = cur.fetchone()[0]
+    await state.update_data(rowid=rowid)
+    await edit_question()
+
+
+@dp.callback_query(F.data == "del_q")
+async def delete_question(callback: types.CallbackQuery, state: FSMContext):
+    builder = InlineKeyboardBuilder()
+    data = await state.get_data()
+    with sq.connect("questions.db") as db:
+        cur = db.cursor()
+        cur.execute("DELETE FROM questions WHERE rowid = ?", (data.get('rowid'),))
+        db.commit()
+    menu_message = "Вопрос удалён"
+    builder.add(types.InlineKeyboardButton(text="Назад", callback_data=f'database_'))
+    await callback.message.answer(menu_message, reply_markup=builder.as_markup())
+    await callback.answer()
+
+
+@dp.callback_query(F.data == "clear_q_flag")
+async def delete_used(callback: types.CallbackQuery, state: FSMContext):
+    builder = InlineKeyboardBuilder()
+    data = await state.get_data()
+    with sq.connect("questions.db") as db:
+        cur = db.cursor()
+        cur.execute("UPDATE questions SET Used = ? WHERE rowid = ?", ('нет', data.get('rowid')))
+        db.commit()
+    menu_message = "Признак сброшен"
+    builder.add(types.InlineKeyboardButton(text="Назад", callback_data=f"question_{data.get('rowid')}"))
+    await callback.message.answer(menu_message, reply_markup=builder.as_markup())
+    await callback.answer()
 
 
 async def main():
